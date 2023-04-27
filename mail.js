421f02a46918abc150858c33f0b286c57ff78a07
Shubham Tribhuvan <106821254+Shubhamtribhuvan8@users.noreply.github.com>
2023-04-27 15:26:39 +0530

    Create mail.js
    mail

---
+++ mail.js
@@ -0,0 +1,66 @@
+function onOpen() {
+  var ui = SpreadsheetApp.getUi();
+  ui.createMenu("Process RFQ")
+    .addItem("Track Sent Email", "getEmailData")
+    .addToUi();
+}
+
+function getEmailData() {
+  var sheet = SpreadsheetApp.getActiveSheet();
+  // var threads = GmailApp.search('in:sent newer_than:1d subject:"RFQ"');//not working
+  // var threads = GmailApp.search('in:sent:2d subject:"RFQ"');//Not working
+  var threads = GmailApp.search('subject:"RFQ"');
+
+
+  var data = [];
+  console.log(data)
+  // Adding the title row
+  var titleRow = ["S. No.", "RFQ Number", "Supplier Name", "Subject", "Email Sent to", "Email Body", "Response (Yes/No)", "Attachments (Yes/No)", "Attachment Link", "Mail Body", "Updated on"];
+  data.push(titleRow);
+  
+  for (var i = 0; i < threads.length; i++) {
+    var messages = threads[i].getMessages();
+    for (var j = 0; j < messages.length; j++) {
+      var message = messages[j];
+      var subject = message.getSubject();
+      var body = message.getPlainBody();
+      var emailSentTo = message.getTo();
+      Logger.log('Processing email with subject: ' + subject);
+      
+      // Extracting RFQ number and Supplier Name from the email subject
+      var subjectRegex = /^RFQ\s+(\d+)\s+(.*?)\s+(\w{3}\s+\d{4})/;
+      var match = subject.match(subjectRegex);
+      if (!match) {
+        Logger.log('No match found for subject: ' + subject);
+        continue;
+      }
+      var rfqNumber = match[1];
+      var supplierName = match[2];
+
+      // Extracting email body
+      var emailBody = body.replace(/^(Supplier Name|Subject):\s+.*/gm, "");
+      // var attachmentLink = attachment.getUrl();
+      
+      
+      // Adding the extracted data to a row
+      var row = [
+        i + 1, // S. No.
+        rfqNumber, // RFQ Number
+        supplierName, // Supplier Name
+        subject, // Subject
+        emailSentTo, // Email Sent to
+        emailBody, // Email Body
+        "", // Response (Yes/No)
+        "", // Attachments (Yes/No)
+        "", // Attachment Link
+        "", // Mail Body
+        "" // Updated on
+      ];
+      data.push(row);
+    }
+  }
+  
+  // Adding the data to the spreadsheet
+  Logger.log('Adding ' + data.length + ' rows to the sheet');
+  sheet.getRange(sheet.getLastRow() + 1, 1, data.length, data[0].length).setValues(data);
+}
